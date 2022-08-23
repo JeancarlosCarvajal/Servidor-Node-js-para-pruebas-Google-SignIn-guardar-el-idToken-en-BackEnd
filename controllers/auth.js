019992cb0c22024bc7de7da6f4bbf7c47460cc03
Jeancarlos  Carvajal <jeancarloscarvajalavila@gmail.com>
2022-08-23 16:46:53 -0400

    Listo toda configuracion del Apple SignIn

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,45 +61,54 @@ const callbackApple = async () => (request, response) => {
 // sign in with apple
 const signInWithApple = async (request, response) => {
 
-  const auth = new AppleAuth(
-    {
-      // use the bundle ID as client ID for native apps, else use the service ID for web-auth flows
-      // https://forums.developer.apple.com/thread/118135
-      client_id:
-        request.query.useBundleId === "true"
-          ? process.env.BUNDLE_ID
-          : process.env.SERVICE_ID,
-      team_id: process.env.TEAM_ID,
-      redirect_uri:
-        "https://google-sign-in-token.herokuapp.com/callbacks/sign_in_with_apple", // does not matter here, as this is already the callback that verifies the token after the redirection
-      key_id: process.env.KEY_ID
-    },
-    fs.readFileSync('./keys/keysignin.p8').toString(),
-    "text"
-    // process.env.KEY_CONTENTS.replace(/\|/g, "\n"),
-    // "text"
-  );
+  try { 
+    const auth = new AppleAuth(
+      {
+        // use the bundle ID as client ID for native apps, else use the service ID for web-auth flows
+        // https://forums.developer.apple.com/thread/118135
+        client_id:
+          request.query.useBundleId === "true"
+            ? process.env.BUNDLE_ID
+            : process.env.SERVICE_ID,
+        team_id: process.env.TEAM_ID,
+        redirect_uri:
+          "https://google-sign-in-token.herokuapp.com/callbacks/sign_in_with_apple", // does not matter here, as this is already the callback that verifies the token after the redirection
+        key_id: process.env.KEY_ID
+      },
+      fs.readFileSync('./keys/keysignin.p8').toString(),
+      "text"
+      // process.env.KEY_CONTENTS.replace(/\|/g, "\n"),
+      // "text"
+    );
 
-  console.log(request.query);
+    console.log(request.query);
 
-  const accessToken = await auth.accessToken(request.query.code);
+    const accessToken = await auth.accessToken(request.query.code);
 
-  const idToken = jwt.decode(accessToken.id_token);
+    const idToken = jwt.decode(accessToken.id_token);
 
-  const userID = idToken.sub;
+    const userID = idToken.sub;
 
-  console.log(idToken);
+    // TODO:  Aqui esta la informaciond el usuario debe guardar en baser de datos para registro
+    console.log(idToken);
 
-  // `userEmail` and `userName` will only be provided for the initial authorization with your app
-  const userEmail = idToken.email;
-  const userName = `${request.query.firstName} ${request.query.lastName}`;
+    // `userEmail` and `userName` will only be provided for the initial authorization with your app
+    const userEmail = idToken.email;
+    const userName = `${request.query.firstName} ${request.query.lastName}`;
 
-  // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
-  const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
+    // 👷🏻‍♀️ TODO: Use the values provided create a new session for the user in your system
+    const sessionID = `NEW SESSION ID for ${userID} / ${userEmail} / ${userName}`;
 
-  console.log(`sessionID = ${sessionID}`);
+    console.log(`sessionID = ${sessionID}`); 
+    response.json({ sessionId: sessionID });
+  } catch (error) {
+    response.json({
+      'Error token Apple: ': error
+    });
+  }
 
-  response.json({ sessionId: sessionID });
+
+
 }
 
 
